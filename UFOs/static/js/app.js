1d4a9ab2bcea79f8b5cbfdca1300d8a2270db5ca
Quinton Butterfield <quintonbutterfield@dhcp-10-101-235-245.wireless.northwestern.private>
2019-06-03 17:31:03 -0500

    last

--- UFOs/static/js/app.js
+++ UFOs/static/js/app.js
@@ -7,7 +7,6 @@ var table = d3.select("table");
 // Use D3 to select the table body
 var tbody = d3.select("tbody");
 
-// function makeTable(){
 data.forEach((ufoSighting) => {
     var row = tbody.append("tr");
     Object.entries(ufoSighting).forEach(([key, value]) => {
@@ -15,7 +14,6 @@ data.forEach((ufoSighting) => {
     cell.text(value);
     });
 });
-// };
 
 var button = d3.select("#filter-btn");
 var inputField = d3.select("#datetime");
@@ -26,14 +24,13 @@ inputField.on("change", function() {
     var sightDate = d3.event.target.value;
     button.on("click", function() {
         function filterDate(sightingDate) {
-            return sightingDate.datetime == sightDate;
+            return sightingDate.datetime === sightDate;
             
           }
-        //   makeTable();
-        
+        tbody.text("")
         var filteredTable = tableData.filter(filterDate);
         tableData = filteredTable;
-        
+
         tableData.forEach((filteredUfoSighting) => {
             var row = tbody.append("tr");
             Object.entries(filteredUfoSighting).forEach(([key, value]) => {
@@ -43,4 +40,3 @@ inputField.on("change", function() {
         });
     });
   });
-// makeTable();
