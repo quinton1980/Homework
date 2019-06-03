c494e502b6d358973108bcd76086cf5dcbc1f0fb
Quinton Butterfield <quintonbutterfield@dhcp-10-101-235-245.wireless.northwestern.private>
2019-06-03 17:15:39 -0500

    adding more work

--- UFOs/static/js/app.js
+++ UFOs/static/js/app.js
@@ -7,6 +7,7 @@ var table = d3.select("table");
 // Use D3 to select the table body
 var tbody = d3.select("tbody");
 
+// function makeTable(){
 data.forEach((ufoSighting) => {
     var row = tbody.append("tr");
     Object.entries(ufoSighting).forEach(([key, value]) => {
@@ -14,29 +15,32 @@ data.forEach((ufoSighting) => {
     cell.text(value);
     });
 });
+// };
 
 var button = d3.select("#filter-btn");
 var inputField = d3.select("#datetime");
 
 
 inputField.on("change", function() {
+    tableData = data;
     var sightDate = d3.event.target.value;
     button.on("click", function() {
         function filterDate(sightingDate) {
             return sightingDate.datetime == sightDate;
+            
           }
+        //   makeTable();
+        
         var filteredTable = tableData.filter(filterDate);
-        console.log(filteredTable);
-
-        var table = d3.select("table");
-        var tbody = d3.select("tbody");
-        filteredTable.forEach((filteredSighting) => {
+        tableData = filteredTable;
+        
+        tableData.forEach((filteredUfoSighting) => {
             var row = tbody.append("tr");
-            Object.entries(filteredSighting).forEach(([key, value]) => {
+            Object.entries(filteredUfoSighting).forEach(([key, value]) => {
             var cell = row.append("td");
             cell.text(value);
             });
         });
-
     });
   });
+// makeTable();
