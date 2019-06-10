3c457719aba217dc9f8efab3ecab96969316194f
Quinton Butterfield <quintonbutterfield@quintons-mbp-2.attlocal.net>
2019-06-09 22:18:49 -0500

    dont get it

--- Belly Button Diversity/StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ Belly Button Diversity/StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -1,8 +1,13 @@
 function buildMetadata(sample) {
 
   // @TODO: Complete the following function that builds the metadata panel
+  var url = "/metadata/<sample>";
 
   // Use `d3.json` to fetch the metadata for a sample
+  d3.json(url).then((response)=>{
+    console.log(response);
+
+  });
     // Use d3 to select the panel with id of `#sample-metadata`
 
     // Use `.html("") to clear any existing metadata
@@ -15,42 +20,42 @@ function buildMetadata(sample) {
     // buildGauge(data.WFREQ);
 }
 
-function buildCharts(sample) {
+// function buildCharts(sample) {
 
-  // @TODO: Use `d3.json` to fetch the sample data for the plots
+//   // @TODO: Use `d3.json` to fetch the sample data for the plots
 
-    // @TODO: Build a Bubble Chart using the sample data
+//     // @TODO: Build a Bubble Chart using the sample data
 
-    // @TODO: Build a Pie Chart
-    // HINT: You will need to use slice() to grab the top 10 sample_values,
-    // otu_ids, and labels (10 each).
-}
+//     // @TODO: Build a Pie Chart
+//     // HINT: You will need to use slice() to grab the top 10 sample_values,
+//     // otu_ids, and labels (10 each).
+// }
 
-function init() {
-  // Grab a reference to the dropdown select element
-  var selector = d3.select("#selDataset");
+// function init() {
+//   // Grab a reference to the dropdown select element
+//   var selector = d3.select("#selDataset");
 
-  // Use the list of sample names to populate the select options
-  d3.json("/names").then((sampleNames) => {
-    sampleNames.forEach((sample) => {
-      selector
-        .append("option")
-        .text(sample)
-        .property("value", sample);
-    });
+//   // Use the list of sample names to populate the select options
+//   d3.json("/names").then((sampleNames) => {
+//     sampleNames.forEach((sample) => {
+//       selector
+//         .append("option")
+//         .text(sample)
+//         .property("value", sample);
+//     });
 
-    // Use the first sample from the list to build the initial plots
-    const firstSample = sampleNames[0];
-    buildCharts(firstSample);
-    buildMetadata(firstSample);
-  });
-}
+//     // Use the first sample from the list to build the initial plots
+//     const firstSample = sampleNames[0];
+//     buildCharts(firstSample);
+//     buildMetadata(firstSample);
+//   });
+// }
 
-function optionChanged(newSample) {
-  // Fetch new data each time a new sample is selected
-  buildCharts(newSample);
-  buildMetadata(newSample);
-}
+// function optionChanged(newSample) {
+//   // Fetch new data each time a new sample is selected
+//   buildCharts(newSample);
+//   buildMetadata(newSample);
+// }
 
-// Initialize the dashboard
-init();
+// // Initialize the dashboard
+// init();
