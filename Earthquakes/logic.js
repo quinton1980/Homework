b4a65b94943e94780a1cdac3f85de48e00bb47ef
Quinton Butterfield <quintonbutterfield@quintons-mbp-2.attlocal.net>
2019-07-11 20:55:36 -0500

    Over it

--- Earthquakes/logic.js
+++ Earthquakes/logic.js
@@ -1,52 +1,71 @@
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
+var lightmap = new L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}?" +
+"access_token=pk.eyJ1IjoicXVpbnRvbjE5ODAiLCJhIjoiY2p3cjE0b2I2MWdrZTQ4bjM3OXVyZG1xMSJ9.MiTO8ooODM2yiraelPhVPA");
 
-// function chooseColor(borough) {
-//   switch (true) {
-//   case ">=5":
-//     return "yellow";
-//   case ">=4":
-//     return "red";
-//   case ">=3":
-//     return "orange";
-//   case ">=2":
-//     return "green";
-//   case "Staten Island":
-//     return "purple";
-//   default:
-//     return "black";
-//   }
-// }
-
-d3.json(queryUrl, function(data) {
-  createFeatures(data.features);
+var myMap = new L.map("map", {
+  center: [49.246292, -123.116226],
+    zoom: 3.5,
+  layers: [lightmap]
 });
 
-function createFeatures(earthquakeData) {
-  function onEachFeature(feature, layer) {
-    layer.bindPopup("<h3>" + feature.properties.place +
-      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson", function(data) {
+  function getStyle(feature) {
+    return {
+      opacity: 1,
+      fillOpacity: 1,
+      fillColor: chooseColor(feature.properties.mag),
+      radius: getRadius(feature.properties.mag),
+      weight: 0.5
+    };
   }
-  var earthquakes = L.geoJSON(earthquakeData, {
-    onEachFeature: onEachFeature
-  });
-  createMap(earthquakes);
+
+
+var earthquakes = new L.LayerGroup();
+
+function chooseColor(magnitude) {
+  switch (true) {
+  case magnitude>=5:
+    return "yellow";
+  case magnitude>=4:
+    return "red";
+  case magnitude>=3:
+    return "orange";
+  case magnitude>=2:
+    return "green";
+  case magnitude>=1:
+    return "purple";
+  default:
+    return "black";
+  }
 }
 
-function createMap(earthquakes) {
-  var lightmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "mapbox.light",
-    accessToken: API_KEY
-  });
-
-  var overlayMaps = {
-    Earthquakes: earthquakes
-  };
-
-  var myMap = L.map("map", {
-    center: [49.246292, -123.116226],
-      zoom: 3.5,
-    layers: [lightmap, earthquakes]
-  });
+function getRadius(magnitude) {
+  if (magnitude === 0) {
+    return 1;
+  }
+  return magnitude * 2.5;
 }
+
+var baseMaps = {
+  Grayscale: lightmap
+};
+
+var overlayMaps = {
+  "Earthquakes": earthquakes
+};
+
+// L.control
+//   .layers(baseMaps, overlayMaps)
+//   .addTo(map);
+ 
+
+
+// L.geoJson(data, {
+//   pointToLayer: function(feature, latlng) {
+//     return L.circleMarker(latlng);
+//   },
+//   style: getStyle,
+//   onEachFeature: function(feature, layer) {
+//     layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+//   }
+// })
+});
